8ede91f76060b374faeed7bdf512e84c7bee9c1f
lucas lucena <lucaslucenacandido@hotmail.com>
2019-03-29 02:09:18 -0300

    Criando menu (Header)

--- ControleEstoque/ControleEstoqueAplication/App_Start/RouteConfig.cs
+++ ControleEstoque/ControleEstoqueAplication/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace ControleEstoqueAplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+                //rota padrão, se não colocar nenhuma URL vai cair nas page HOME e INDEX ou os 2 juntos
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
--- ControleEstoque/ControleEstoqueAplication/Controllers/HomeController.cs
+++ ControleEstoque/ControleEstoqueAplication/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace ControleEstoqueAplication.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Sobre()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
